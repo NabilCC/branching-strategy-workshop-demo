1fd74376fb61e6a59582e7fbca276c4cebf48bf7
Nabil Hassan <nabil.hassan@clearchannel.co.uk>
2020-05-10 22:44:45 +0100

    Add feature not enabled constant

--- src/main/java/com/ccuk/demo/api/ApplicationController.java
+++ src/main/java/com/ccuk/demo/api/ApplicationController.java
@@ -16,6 +16,7 @@ import java.security.Principal;
 public class ApplicationController {
 
     private static final Logger LOG = LoggerFactory.getLogger(ApplicationController.class);
+    private static final String FEATURE_NOT_ENABLED_FOR_USER = "This feature is not enabled for the specified user";
 
     private FeatureFlagService featureFlagService;
     private MaintenanceService maintenanceService;
@@ -29,7 +30,7 @@ public class ApplicationController {
     @GetMapping("/")
     public String testEndpoint(Principal principal) {
         boolean enabled = featureFlagService.isFeatureEnabledForUser(principal, FeatureFlag.TEST_ENDPOINT);
-        return enabled ? maintenanceService.welcomeMessage() : "This feature is not enabled";
+        return enabled ? maintenanceService.welcomeMessage() : FEATURE_NOT_ENABLED_FOR_USER;
     }
 
 }
