66a04f8bfef3037ba0319395ef98037ebae49667
Nabil Hassan <nabil.hassan@clearchannel.co.uk>
2020-05-22 11:41:07 +0100

    Change create instructions feature flag value

--- src/main/java/com/ccuk/demo/feature/FeatureFlag.java
+++ src/main/java/com/ccuk/demo/feature/FeatureFlag.java
@@ -2,7 +2,7 @@ package com.ccuk.demo.feature;
 
 public enum FeatureFlag {
 
-    CREATE_INSTRUCTION("create.instruction"), GET_INSTRUCTION_BY_ID("get.instruction.by.id"), GET_INSTRUCTION_BY_ASSIGNEE("get.instruction.by.assignee"), TEST_ENDPOINT("test.endpoint");
+    CREATE_INSTRUCTION("create.instructions"), GET_INSTRUCTION_BY_ID("get.instruction.by.id"), GET_INSTRUCTION_BY_ASSIGNEE("get.instruction.by.assignee"), TEST_ENDPOINT("test.endpoint");
 
     String key;
 
